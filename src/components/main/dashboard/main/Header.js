83a4b5ac84495ace7b6898458eb2b4ad1c755dda
Alex Bogomilov <alexbogomilov772gmail.com>
2020-06-25 23:16:41 +0300

    fix: progress

--- src/components/main/dashboard/main/Header.js
+++ src/components/main/dashboard/main/Header.js
@@ -6,30 +6,33 @@ class Header extends Component {
     return (
       <div className="dashboard-main-header">
           {/* <p className="label">Cleaning DPF</p> */}
-          <p className="label">Black Smoke</p>
-          {/* <div className="toggle-action">
-            <span className="action active-action">Request</span>
-            <span className="divider-action"> / </span>
-            <span  className="action">Diagnose</span>
-          </div> */}          
-          <div className="toggle-action">
-            <span className="action active-action">Service</span>
-            <span className="divider-action"> / </span>
-            <span  className="action">Parts</span>
-          </div>
           <div className="toggle-action2">
             <div className="left">
               <div className="btn-switch btn-switch-left btn-active">
-                ❖
+                {/* ❖ */}
               </div>
               {/* <span className="btn-switch-label">request</span> */}
             </div>
             <div className="right">
               <div className="btn-switch btn-switch-right">
-                ❖
+                {/* ❖ */}
               </div>
               {/* <span className="btn-switch-label">diagnose</span> */}
             </div>
+          </div>
+
+
+          <span className="label">Turbo Charging</span>
+          {/* <div className="toggle-action">
+            <span className="action active-action">Request</span>
+            <span className="divider-action"> / </span>
+            <span  className="action">Diagnose</span>
+          </div> */}
+
+          <div className="toggle-action">
+            <span className="action active-action">Service</span>
+            {/* <span className="divider-action"> / </span> */}
+            {/* <span  className="action">Parts</span> */}
           </div>
       </div>
     )
