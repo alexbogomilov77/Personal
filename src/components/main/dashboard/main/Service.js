880605dbe67d560f08cde6cb30560b3c81028598
Alex Bogomilov <alexbogomilov772gmail.com>
2020-06-27 20:48:40 +0300

    feat: design update

--- src/components/main/dashboard/main/Service.js
+++ src/components/main/dashboard/main/Service.js
@@ -6,7 +6,22 @@ class Service extends Component {
     return (
       <div className="dashboard-main-service">
 
+        <div className="service-input-fields">
+          <div className="input-block">
+            <div className="input">
+              <label className="input-label" htmlFor="name">Name:</label>
+              <input type="text" id="name"  name="name" />
+            </div>
+          </div>
 
+          <div className="input-block">
+            <label className="input-label" htmlFor="value">Price:</label>
+            <div className="input">
+              <input type="text" id="value" name="value" />
+            </div>
+          </div>
+          <button className="add btn-active2"></button>
+        </div>
         
 
         <div className='service-list-wrapper'>
@@ -29,23 +44,6 @@ class Service extends Component {
             </li>
           </ul>
         </div>
-
-        <div className="service-input-fields">
-        <div className="input-block">
-          <label className="input-label" htmlFor="name">Name:</label>
-          <div className="input">
-            <input type="text" id="name" name="name" />
-          </div>
-        </div>
-
-        <div className="input-block">
-          <label className="input-label" htmlFor="value">Price:</label>
-          <div className="input">
-            <input type="text" id="value" name="value" />
-            <button className="add"></button>
-          </div>
-        </div>
-      </div>
 
         <div className="dashboard-main-service-footer">
           <button className="action-btn btn-active">complete</button>
