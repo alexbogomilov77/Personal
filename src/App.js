ce68907195ea7106f5f1a2f183f91cab2d201252
Alex Bogomilov <alexbogomilov772gmail.com>
2021-02-07 21:02:04 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 //components
 import Main from './components/global/Main'
 import Header from './components/global/Header'
-// import Search from './components/global/Search'
 import Sidebar from './components/global/Sidebar'
-
 //styles
 import './assets/styles/index.scss'
+import './App.scss'
+import axios from 'axios'
 
-class App extends Component {
+export const CarsContext = React.createContext()
 
-  render() {
-    return (
-      <div className="App">
+export default function App() {
+
+  useEffect(() => {
+    selectTab(0)
+    fetchCars()
+  },[])
+
+  const [cars, setCars] = useState([])
+  const [selectedTab, setSelectedTab] = useState(null)
+
+  const fetchCars = () => {
+    axios.get('http://localhost:8080/cars')
+    .then(response => {
+      const selectedTab = Number(localStorage.getItem('sidebarTab'))
+      const carsInSelectedTab = response.data.filter(el => el.state === selectedTab)
+      setCars(carsInSelectedTab)
+    })
+  }
+
+  const selectTab = value => {
+    localStorage.setItem('sidebarTab', value)
+    setSelectedTab(value)
+    fetchCars()
+  }
+
+  return (
+    <div className="App">
+      <CarsContext.Provider value={cars}>
         <Sidebar />
         <Header />
-        {/* <Search /> */}
         <Main />
-      </div>
-    )
-  }
+      </CarsContext.Provider>
+    </div>
+  )
 }
-
-export default App
--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -1,184 +1,162 @@
-import React, { Component } from 'react'
+import React, { useContext, useState } from 'react'
 import logo from '../../assets/images/dd2.jpg'
 import ReactModal from 'react-modal';
 import Modal from '../modals/Modal';
-
+import { CarsContext } from '../../App'
 import axios from 'axios'
 
 //redux
-import { connect } from 'react-redux'
-import { getCars } from '../../store/actions/getCarsActions'
-import { getRepairs } from '../../store/actions/getRepairsActions'
+// import { connect } from 'react-redux'
+// import { getCars } from '../../store/actions/getCarsActions'
+// import { getRepairs } from '../../store/actions/getRepairsActions'
 
 
-class Sidebar extends Component {
+export default function Sidebar () {
+  const cars = useContext(CarsContext)
 
-  state = {
-    showModal: false,
-    activeLink: null,
-    selectedTab: null,
-    isCarExist: false,
-    searchInputValue: '',
-    isSearchLegit: false
-  }
+  const [showModal, setModal] = useState(false)
+  const [activeLink, setActiveLink] = useState(null)
+  const [selectedTab, setSelectedTab] = useState(null)
+  const [isCarExist, setIsCarExist] = useState(false)
+  const [searchInputValue, setSearchInputValue] = useState('')
+  const [isSearchLegit, setIsSearchLegit] = useState(false)
 
-  componentDidMount() {
-    this.selectTab(0)
-    // test
-  }
+  // componentDidMount() {
+  //   this.selectTab(0)
+  // }
 
-  selectTab = value => {
-    localStorage.setItem('sidebarTab', value)
-    this.selectedTab = value
-    this.props.getCars();
-  }
+  // const selectTab = value => {
+  //   selectTab = value => {
+  //     localStorage.setItem('sidebarTab', value)
+  //     this.selectedTab = value
+  //     this.props.getCars();
+  //   }
+  // }
 
-  handleClick = id => {
-    this.setState({ activeLink: id });
+  const handleClick = id => {
+    setActiveLink(id)
     this.props.getRepairs(id);
   }
 
-  handleResponse = response => {
-    this.setState({ isCarExist: response });
+  const changeStatus = (plate, value) => {
+    axios.post(`http://localhost:8080/cars/${plate}`, {status: value})
   }
 
-  changeStatus = (plate, value) => {
-    alert('test')
-    axios.post(`http://localhost:8080/cars/${plate}`, {status: value}).then( response => {
-      console.log(response)
-    })
+  const handleInput = event => {
+    event.target.value.length === 8
+    ? setIsSearchLegit(true)
+    : setIsSearchLegit(false)
   }
 
-  handleInput = event => {
-    console.log(event.target.value.length)
-    if (event.target.value.length === 8) {
-      this.setState({ isSearchLegit: true })
-    } else {
-      this.setState({ isSearchLegit: false })
-    }
-  }
-
-  handleKeyUp = event => {
+  const handleKeyUp = event => {
     if (event.keyCode === 13) {
       event.preventDefault();
-      this.setState({ searchInputValue: event.target.value });
-      axios.get(`http://localhost:8080/cars/search/${this.state.searchInputValue}`)
+      searchInputValue(event.target.value)
+      axios.get(`http://localhost:8080/cars/search/${searchInputValue}`)
       .then( response => {
-        response.data.length > 0 ? this.handleResponse(true) : this.handleResponse(false)
-      }).catch((err) => {
+        response.data.length > 0
+        ? setIsCarExist(true)
+        : setIsCarExist(false)
+      }).catch( err => {
         alert(err)
       })
     }
   }
 
-  handleOpenModal = () => {
-    this.setState({ showModal: true });
+  const handleOpenModal = () => {
+    setModal(true)
   }
-  
-  handleCloseModal = () => {
-    this.setState({ showModal: false });
+  const handleCloseModal = () => {
+    setModal(false)
   }
 
-  render() {
-    const { activeLink, isCarExist, isSearchLegit, searchInputValue } = this.state;
-    let buttons;
-    // if (!isSearchLegit) { !isSearchLegit !isCarExist
-        buttons =
+  // return (
+  //   let buttons;
+  //   if (!isSearchLegit) { !isSearchLegit !isCarExist
+  //       buttons =
+  //       <div className="actionButtons">
+  //         <button disabled={!isSearchLegit && isCarExist} className="actionBtn">Add</button>
+  //         <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => this.changeStatus(searchInputValue, 0)}>A</button>
+  //         <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => this.changeStatus(searchInputValue, 1)}>W</button>
+  //         <button disabled={!isSearchLegit && isCarExist} className="actionBtn">R</button>
+  //       </div>
+  //   }
+
+  return cars ? (
+    <div className='sidebar'>
+
+      <div className="logo">
+        <img className="logo-image" src={logo}></img>
+        <p className="logo-label">GaragePanel</p>
+      </div>
+
+      <div className="searchField">
+        <input
+          type="search"
+          className="searchInput"
+          name="q"
+          placeholder="Search in your garage"
+          onChange={handleInput}
+          onKeyUp={handleKeyUp}
+        />
         <div className="actionButtons">
           <button disabled={!isSearchLegit && isCarExist} className="actionBtn">Add</button>
-          <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => this.changeStatus(searchInputValue, 0)}>A</button>
-          <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => this.changeStatus(searchInputValue, 1)}>W</button>
+          <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => changeStatus(searchInputValue, 0)}>A</button>
+          <button disabled={!isSearchLegit && !isCarExist} className="actionBtn" onClick={() => changeStatus(searchInputValue, 1)}>W</button>
           <button disabled={!isSearchLegit && isCarExist} className="actionBtn">R</button>
         </div>
-    // }
+      </div>
+      
 
-    return this.props.cars.length > 0 ? (
-      <div className='sidebar'>
+      {/* <button 
+        onClick={this.handleOpenModal}
+        className='new btn btn-active'>
+        New
+      </button> */}
 
-        <div className="logo">
-          <img className="logo-image" src={logo}></img>
-          <p className="logo-label">GaragePanel</p>
-        </div>
-
-        <div className="searchField">
-          <input
-            type="search"
-            className="searchInput"
-            name="q"
-            placeholder="Search in your garage"
-            onChange={this.handleInput}
-            onKeyUp={this.handleKeyUp}
-          />
-          {buttons}
-        </div>
-        
-
-        {/* <button 
-          onClick={this.handleOpenModal}
-          className='new btn btn-active'>
-          New
-        </button> */}
-
-        <div className='categories'>
-        <span
-            className={'category ' + (this.selectedTab === 0 ? 'active-category': '')}
-            onClick={() => this.selectTab(0)}
-          >
-            active
-          </span>
-          <span className='divider'> / </span>
-          <span
-            className={'category ' + (this.selectedTab === 1 ? 'active-category': '')}
-            onClick={() => this.selectTab(1)}
-          >
-            waiting
-          </span>
-        </div>
-
-        <ul className='sidebar-list'>
-          {this.props.cars.map((car) => {
-            return (
-              <li
-                key={car.vehicle_id}
-                onClick={() => this.handleClick(car.vehicle_id)}
-                className={'sidebar-item ' + (car.vehicle_id === activeLink ? 'active-item': '')}>
-                {car.plate}
-              </li>
-            );
-          })}
-        </ul>
-
-        <ReactModal 
-          className="modal-wrapper"
-          overlayClassName="modal-overlay"
-          isOpen={this.state.showModal}
-          contentLabel="onRequestClose Example"
-          onRequestClose={this.handleCloseModal}
+      <div className='categories'>
+      <span
+          className={'category ' + (selectedTab === 0 ? 'active-category': '')}
+          onClick={() => setSelectedTab(0)}
         >
-          <Modal />
-          <button onClick={this.handleCloseModal}>Close Modal</button>
-        </ReactModal>
+          active
+        </span>
+        <span className='divider'> / </span>
+        <span
+          className={'category ' + (selectedTab === 1 ? 'active-category': '')}
+          onClick={() => setSelectedTab(1)}
+        >
+          waiting
+        </span>
       </div>
-    ) : (
-      <div>
-        <code>no cars in your garage!</code>
-      </div>
-    )
-  }
+
+      <ul className='sidebar-list'>
+        {cars.map(car => {
+          return (
+            <li
+              key={car.vehicle_id}
+              onClick={() => handleClick(car.vehicle_id)}
+              className={'sidebar-item ' + (car.vehicle_id === activeLink ? 'active-item': '')}>
+              {car.plate}
+            </li>
+          );
+        })}
+      </ul>
+
+      <ReactModal 
+        className="modal-wrapper"
+        overlayClassName="modal-overlay"
+        isOpen={showModal}
+        contentLabel="onRequestClose Example"
+        onRequestClose={handleCloseModal}
+      >
+        <Modal />
+        <button onClick={handleCloseModal}>Close Modal</button>
+      </ReactModal>
+    </div>
+  ) : (
+    <div>
+      <code>no cars in your garage!</code>
+    </div>
+  )
 }
-
-const mapStateToProps = (state) => {
-  return {
-    cars: state.getCars.listOfCars
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    getCars: () => dispatch(getCars()),
-    getRepairs: (id) => dispatch(getRepairs(id))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
-
