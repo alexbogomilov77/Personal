3b26c873dd15e1069b897c38373447628dd4d611
Alexander Bogomilov <alexanderbogomilov@Alexanders-MacBook-Pro-2.local>
2022-05-11 12:37:40 +0300

    migrate

--- frontend/src/utils/dates.js
+++
@@ -1,6 +0,0 @@
-export const getDate = (el) => {
-  const day = new Date(el.start_date).getUTCDate();
-  const month = new Date(el.start_date).getUTCMonth();
-  const year = new Date(el.start_date).getUTCFullYear();
-  return `${day}.${month}.${year}`;
-};
